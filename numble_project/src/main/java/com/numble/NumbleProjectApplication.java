2bc8bb9e528ec501f4755236a5c3164980ddbe9d
eunjin3481 <eunjin3481@gmail.com>
2023-07-10 12:36:11 +0900

    프로젝트 생성

---
+++ numble_project/src/main/java/com/numble/NumbleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.numble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumbleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumbleProjectApplication.class, args);
+	}
+
+}
---
+++ numble_project/src/test/java/com/numble/NumbleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.numble;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumbleProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
