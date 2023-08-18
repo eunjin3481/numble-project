de5831f52c89bb55458d8d1dcc1e3c8596396a2e
eunjin3481 <eunjin3481@gmail.com>
2023-08-18 18:22:32 +0900

    remove

--- numble_project/src/main/java/com/numble/persistence/UserDAO.java
+++
@@ -1,9 +0,0 @@
-package com.numble.persistence;
-
-import com.numble.domain.UserVO;
-
-public interface UserDAO {
-	public void add(UserVO newUser) throws Exception; 
-	public void delete(int userId) throws Exception;
-	public boolean findById(int userId) throws Exception;
-}
